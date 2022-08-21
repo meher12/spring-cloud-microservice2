f49ed63cd29562110d2e7b2ec73a2a5757c3ce92
meher12 <meher.khemiri@gmail.com>
2022-08-21 20:27:27 +0100

    update fraud project

--- fraud/src/main/java/org/fraud/model/Fraud.java
+++ fraud/src/main/java/org/fraud/model/Fraud.java
@@ -1,6 +1,12 @@
 package org.fraud.model;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -9,7 +15,23 @@ import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Fraud {
+    
+    @Id
+    @SequenceGenerator(
+            name = "fraud_id_sequence",
+            sequenceName = "fraud_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "fraud_id_sequence"
+    )
+    private Integer id;
+    private Integer customerId;
+    private Boolean isFraudster;
+    private LocalDateTime createdAt;
 
 }
